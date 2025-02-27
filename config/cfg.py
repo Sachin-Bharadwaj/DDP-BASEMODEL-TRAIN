0d0452c09cd35345ad2d125c1816f75101084dd0
Sachin Bharadwaj <sachin.bharadwaj@gmail.com>
2025-02-27 10:21:30 +0530

    updated config with dead code

--- config/cfg.py
+++ config/cfg.py
@@ -15,6 +15,22 @@ class GPT2Config:
     rope_en: bool = field(default=False)
     theta_ref:int = field(default=50000) # only valid when rope_en is TRUE
 
+if 0:
+    @dataclass(frozen=True)
+    class GPT2Config:
+        '''
+        This will be vanilla GPT Config with fixed Positional Encoding, Causal MHA
+        '''
+        vocab_size:int = field(default=50257)
+        context_length:int = field(default=1024)
+        emb_dim:int = field(default=1032)
+        n_heads:int = field(default=24)
+        n_layers:int = field(default=24)
+        drop_rate:float = field(default=0.1)
+        qkv_bias:bool = field(default=False)
+        rope_en: bool = field(default=False)
+        theta_ref:int = field(default=50000) # only valid when rope_en is TRUE
+
 
 if __name__ == "__main__":
     gpt2cfg = GPT2Config()
